4bf2137c2d2dcf4f86025b5cf0370c0cf614e1e5
Katerina <ekaterinalf@mail.ru>
2022-07-13 20:34:35 +0300

    feat: decode function

--- src/index.js
+++ src/index.js
@@ -39,6 +39,25 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let morseArr = []
+
+    for(let i = 0; i < expr.length; i += 10) {
+      let substr = expr.substr(i, 10)
+      if(substr === '**********') {
+        morseArr.push(' ')
+      } else {
+        let letter = ''
+        for(let j = 0; j < substr.length; j += 2) {
+          substr[j]+substr[j + 1] === '10' ? letter += '.' : substr[j]+substr[j + 1] === '11' ? letter += '-' : null
+        }
+        morseArr.push(letter)
+      }
+    }
+
+    let result = []
+    morseArr.forEach(el => el === ' '? result.push(' ') : result.push(MORSE_TABLE[el]))
+
+    return result.join('')
 }
 
 module.exports = {
